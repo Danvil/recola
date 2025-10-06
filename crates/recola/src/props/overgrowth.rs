22abc730a2735293bdd6d588b64b7a4fa31cd2ee
David Weikersdorfer <517608+Danvil@users.noreply.github.com>
2025-10-06 12:18:31 -0700

    recola: Overgrowth burn effect

--- crates/recola/src/props/overgrowth.rs
+++ crates/recola/src/props/overgrowth.rs
@@ -1,9 +1,12 @@
-use crate::props::laser_pointer::*;
+use crate::{Rng, props::laser_pointer::*};
 use candy::MaterialDirty;
+use candy_mesh::Cuboid;
+use candy_scene_tree::{GlobalTransform3, Transform3, Visibility};
 use candy_time::{CandyTimeMocca, SimClock};
 use candy_utils::{Material, PbrMaterial};
 use excess::prelude::*;
-use magi_color::{LinearColor, SRgbU8Color};
+use glam::Vec3;
+use magi_color::{LinearColor, SRgbU8Color, colors};
 use simplecs::prelude::*;
 
 #[derive(Component)]
@@ -14,6 +17,7 @@ pub struct InitOvergrowthTask {
 #[derive(Component)]
 pub struct Overgrowth {
     pub burn_progress: f32,
+    pub burn_particle_gen: f32,
     pub change_mat_entity: Entity,
 }
 
@@ -35,11 +39,14 @@ impl Mocca for OvergrowthMocca {
     fn register_components(world: &mut World) {
         world.register_component::<InitOvergrowthTask>();
         world.register_component::<Overgrowth>();
+        world.register_component::<OvergrowthBurnParticle>();
     }
 
     fn step(&mut self, world: &mut World) {
         world.run(init_overgrowth);
         world.run(burn_overgrowth);
+        world.run(spawn_overgrowth_burn_particles);
+        world.run(animate_overgrowth_burn_particles);
     }
 }
 
@@ -49,6 +56,7 @@ fn init_overgrowth(mut cmd: Commands, query_task: Query<(Entity, &InitOvergrowth
         cmd.entity(entity)
             .and_set(Overgrowth {
                 burn_progress: 0.,
+                burn_particle_gen: 0.,
                 change_mat_entity: task.change_mat_entity,
             })
             .and_set(BeamDetector { latch: false });
@@ -66,6 +74,8 @@ fn burn_overgrowth(
     let dt = time.sim_dt_f32();
     for (entity, overgrowth, hit) in query.iter_mut() {
         if hit.as_bool() {
+            overgrowth.burn_particle_gen += dt;
+
             overgrowth.burn_progress += dt;
             let q = overgrowth.burn_progress / OVERGROWTH_BURN_DURATION;
 
@@ -80,5 +90,88 @@ fn burn_overgrowth(
                 .and_set(Material::Pbr(mat))
                 .and_set(MaterialDirty);
         }
+    }
+}
+
+#[derive(Component)]
+struct OvergrowthBurnParticle {
+    age: f32,
+}
+
+const OVERGROWTH_BURN_PARTICLE_SPAWN_RATE: f32 = 0.0133;
+const OVERGROWTH_BURN_SPAWN_BOX: Vec3 = Vec3::new(3.0, 0.3, 6.0);
+const OVERGROWTH_BURN_PARTICLE_MAX_AGE: f32 = 1.333;
+const OVERGROWTH_BURN_PARTICLE_SIZE: f32 = 0.2;
+const OVERGROWTH_BURN_PARTICLE_SPEED: f32 = 3.333;
+const OVERGROWTH_BURN_PARTICLE_AGE_SIZE_PEAK: f32 = 0.96;
+
+fn spawn_overgrowth_burn_particles(
+    mut cmd: Commands,
+    mut rng: SingletonMut<Rng>,
+    mut query: Query<(&GlobalTransform3, &mut Overgrowth)>,
+) {
+    for (tf, overgrowth) in query.iter_mut() {
+        while overgrowth.burn_particle_gen >= OVERGROWTH_BURN_PARTICLE_SPAWN_RATE {
+            overgrowth.burn_particle_gen -= OVERGROWTH_BURN_PARTICLE_SPAWN_RATE;
+
+            cmd.spawn((
+                OvergrowthBurnParticle { age: 0. },
+                Cuboid,
+                Material::Pbr(PbrMaterial::default().with_base_color(colors::BLACK)),
+                Visibility::Visible,
+                Transform3::identity()
+                    .with_translation(
+                        tf.transform_point3(
+                            rng.uniform_vec3(Vec3::ZERO, OVERGROWTH_BURN_SPAWN_BOX),
+                        ),
+                    )
+                    .with_rotation(rng.uniform_so3())
+                    .with_scale_uniform(0.),
+            ));
+        }
+    }
+}
+
+fn animate_overgrowth_burn_particles(
+    mut cmd: Commands,
+    time: Singleton<SimClock>,
+    mut query: Query<(Entity, &mut OvergrowthBurnParticle, &mut Transform3)>,
+) {
+    let color_1: LinearColor = SRgbU8Color::from_rgb(255, 242, 156).to_linear();
+    let color_2_age_q = 0.133;
+    let color_2: LinearColor = SRgbU8Color::from_rgb(240, 97, 26).to_linear();
+    let color_3: LinearColor = SRgbU8Color::from_rgb(23, 23, 23).to_linear();
+
+    let dt = time.sim_dt_f32();
+    let step = dt * OVERGROWTH_BURN_PARTICLE_SPEED;
+
+    for (entity, particle, tf) in query.iter_mut() {
+        particle.age += dt;
+        let rel_age = particle.age / OVERGROWTH_BURN_PARTICLE_MAX_AGE;
+        if rel_age >= 1. {
+            cmd.despawn(entity);
+            continue;
+        }
+
+        let age_size = if rel_age < OVERGROWTH_BURN_PARTICLE_AGE_SIZE_PEAK {
+            rel_age / OVERGROWTH_BURN_PARTICLE_AGE_SIZE_PEAK
+        } else {
+            1.0 - (rel_age - OVERGROWTH_BURN_PARTICLE_AGE_SIZE_PEAK)
+                / (1.0 - OVERGROWTH_BURN_PARTICLE_AGE_SIZE_PEAK)
+        };
+        tf.scale = Vec3::splat(OVERGROWTH_BURN_PARTICLE_SIZE * age_size);
+
+        let age_color = if rel_age < color_2_age_q {
+            color_1.mix(rel_age / color_2_age_q, color_2)
+        } else {
+            color_2.mix((rel_age - color_2_age_q) / (1. - color_2_age_q), color_3)
+        };
+        let mat = PbrMaterial::diffuse_white().with_base_color(age_color);
+
+        cmd.entity(entity)
+            .and_set(Material::Pbr(mat))
+            .and_set(MaterialDirty);
+
+        tf.translation.z += step;
     }
 }
